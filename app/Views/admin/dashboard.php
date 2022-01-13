54d17432c6c04014b29f55d6630b901f348a75af
pharsaliaa <aaldyrizkipermadi@student.telkomuniversity.ac.id>
2022-01-13 23:25:33 +0700

    Change Profile Name to Session

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -72,7 +72,7 @@
                                 <a class="nav-link" href="javascript:void(0)" role="button" data-toggle="dropdown">
                                     <img src="<?= base_url() ?>/assets/images/profile/17.jpg" width="20" alt=""/>
 									<div class="header-info">
-										<span class="text-black"><strong>Peter Parkur</strong></span>
+										<span class="text-black"><strong><?= session()->get('fname');?> <?= session()->get('lname');?></strong></span>
 										<p class="fs-12 mb-0">Super Admin</p>
 									</div>
                                 </a>
